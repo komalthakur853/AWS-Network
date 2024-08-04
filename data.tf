00a89ef3eb25609dec876f22cee457aa77e10bd6
Komal Jaiswal <99614372+komalthakur853@users.noreply.github.com>
2024-08-04 17:47:13 +0530

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_ami" "redis" {
+  most_recent = true
+  owners      = ["self"] # Assuming you have a custom Redis AMI
+
+  filter {
+    name   = "name"
+    values = ["redis-ubuntu-*"] # Adjust this based on your Redis AMI naming convention
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
